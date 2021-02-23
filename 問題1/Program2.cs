be6e683cdfc7a802757541f48ea2f3d5b001c562
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-02-24 02:19:21 +0900

    prob2-2.(難易度：★)

---
+++ 問題1/Program2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Problem2_2
+{
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            int a = 4, b = 2;
+            Console.WriteLine("a = {0}", a);
+            Console.WriteLine("b = {0}", b);
+            Console.WriteLine("a + b = {0}", a + b);
+            Console.WriteLine("a + b = {0}", a - b);
+            Console.WriteLine("a + b = {0}", a * b);
+            Console.WriteLine("a + b = {0}", a / b);
+            Console.WriteLine("a + b = {0}", a % b);
+        }
+    }
+}
