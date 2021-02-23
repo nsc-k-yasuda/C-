1c41cfb6951b8976454999f838b98b2e66d2a90d
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-02-24 08:54:15 +0900

    prob3-5

---
+++ Program10.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace prob3_4
+{
+    class Program9
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("整数値を入力:");
+            int a = int.Parse(Console.ReadLine());
+            if (a >30 || a<=70)
+            {
+                Console.WriteLine("30より大きく70以下です");
+            }
+        }
+    }
+}
