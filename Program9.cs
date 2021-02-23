d3056be8d57fc125cde1a0e923fbc0c04a42e06e
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-02-24 08:52:04 +0900

    prob3-4.(難易度：★)

---
+++ Program9.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace prob3_4
+{
+    class Program9
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("整数値を入力:");
+            int a = int.Parse(Console.ReadLine());
+            if (a <=20 || a>=80)
+            {
+                Console.WriteLine("20以下か80以上の値です");
+            }
+        }
+    }
+}
