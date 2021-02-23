975ea506a2197a0f7cdbbb41bee6f1011985e138
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-02-23 20:39:01 +0900

    C#　prob1-1.(難易度：★)

---
+++ 問題1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample1
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("山田　花子");
+        }
+    }
+}
