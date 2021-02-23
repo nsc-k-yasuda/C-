df63ca97f288de6fa9cbd1b7f2c39810fa54b45d
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-02-24 08:35:38 +0900

    prob3-1.(難易度：★)

---
+++ Program6.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace prob3_1
+{
+    class Program6
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("整数値を入力:");
+            int a = int.Parse(Console.ReadLine());
+            if (a==3)
+            {
+                Console.WriteLine("3です"); 
+            }
+        }
+    }
+}
