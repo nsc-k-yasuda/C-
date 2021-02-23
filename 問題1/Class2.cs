65ac0b654485f60d2078c6c25df83273a09f9dbc
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-02-24 00:59:52 +0900

    prob1-2.(難易度：★)

---
+++ 問題1/Class2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Class2
+{
+	public Class2()
+	{
+        static void Main(string[] args)
+        {
+            Console.Write("123456789");
+            
+        }
+    }
+}
