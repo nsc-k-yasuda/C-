1194011160cc2c0a1b88065c35167c2dc030a940
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-02-24 08:45:39 +0900

    prob3-4.(難易度：★)

---
+++ Program8.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace prob3_3
+{
+    class Program8
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("整数値を入力:");
+            int a = int.Parse(Console.ReadLine());
+            if (a < 50)
+            {
+                Console.WriteLine("50未満です。");
+            }
+        }
+    }
+}
