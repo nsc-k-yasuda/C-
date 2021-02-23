9bc0e6d4990e7ee348892093d7efb62b89e06237
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-02-24 01:59:24 +0900

    prob1-3.(難易度：★)

--- 問題1/Program.cs
+++ 問題1/Program.cs
@@ -1,23 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-
-
-namespace Sample1
+namespace Problem1_3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("{0} + {1} = {2}", 5, 3, 5 + 3);
-            Console.WriteLine("{0} + {1} = {2}", 5, 2, 5 - 2);
-            Console.WriteLine("{0} + {1} = {2}", 5, 2, 5 * 2);
-            Console.WriteLine("{0} + {1} = {2}", 5, 2, 5 / 2);
-            Console.WriteLine("{0} + {1} = {2}", 5, 2, 5 % 2);
+            Console.WriteLine("こんにちは");
         }
     }
 }
-
