e42bf16f545e4208f425a066fa986c52d3c311cd
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-02-24 08:40:15 +0900

    prob3-2.(難易度：★)

---
+++ Program7.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace prob3_2
+{
+    class Program6
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("整数値を入力:");
+            int a = int.Parse(Console.ReadLine());
+            if (a != 4)
+            {
+                Console.WriteLine("4ではありません。");
+            }
+        }
+    }
+}
