bbe8c5ce4c5d252a63f498739a2e0b869acd3b08
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-02-24 02:34:49 +0900

    prob2-3.(難易度：★)

---
+++ 問題1/Program4.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace prob2_3
+{
+    class Program4
+    {
+        static void Main(string[] args)
+        {
+            string str1;
+            Console.Write("文字列を入力：");
+            str1 = Console.ReadLine();  //  １つ目の文字列を入力
+            Console.WriteLine("入力された文字列:"+ str1);
+        }
+    }
+}
