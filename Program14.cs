22352ad5789228200d6a6889ee64e20ea160546c
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-02-25 08:39:09 +0900

    prob3-9.(難易度：★)2/25

---
+++ Program14.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace prob3_7
+{
+    class Program12
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("文字列を入力:");
+            var str = Console.ReadLine();
+            String str = "abc";
+
+            if (str1)
+            {
+                Console.WriteLine("abcです。");
+            }
+            else
+            {
+                Console.WriteLine("abcではありません。");
+            }
+        }
+    }
+}
