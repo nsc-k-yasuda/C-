d18a451a7e040383f9e43b75f99d809afd7cbf7e
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-02-24 09:00:17 +0900

    prob3-6.(難易度：★★)

---
+++ Program11.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace prob3_6
+{
+    class Program11
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a=");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("b=");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("a={0} b={1}", a, b);
+            if (a == b)
+            {
+                Console.WriteLine("aとbと等しい");
+            }else if (a>b)
+            {
+                Console.WriteLine("aはbよりも大きい");
+            }
+            else if (a < b)
+            {
+                Console.WriteLine("aはbよりも小さい");
+            }
+        }
+    }
+}
