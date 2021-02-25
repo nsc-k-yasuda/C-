c8ae4ceb881fd2a766a0e7acb90441e7dc6943e2
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-02-26 02:19:20 +0900

    prob3-12.(難易度：★)2/26

---
+++ Program17.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace prob3_12
+{
+    class Program17
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("整数を入力:");
+            int num = int.Parse(Console.ReadLine());
+            if (num > 0)
+            {
+                Console.WriteLine("正の値です。");
+            }else if (num < 0)
+            {
+                Console.WriteLine("負の値です。");
+            }
+            else if (num == 0)
+            {
+                Console.WriteLine("0です。");
+            }
+        }
+    }
+}
