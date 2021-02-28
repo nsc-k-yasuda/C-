f293ed9cc8d995c0b2559b39f79ef47378667cac
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-02-28 21:19:22 +0900

    prob3-15.(難易度：★★)2/28

---
+++ Program20.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace prob3_15
+{
+    class Program20
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("点数(1-100)：");
+            int suuji = int.Parse(Console.ReadLine());
+
+            if (suuji <0 || suuji > 100)
+            {
+                Console.WriteLine("範囲外");
+            }
+            else if(suuji >= 80)
+            {
+                Console.WriteLine("優");
+            }
+            else if (suuji >= 70 && suuji <80)
+            {
+                Console.WriteLine("良");
+            }
+            else if (suuji < 70 && suuji >= 60)
+            {
+                Console.WriteLine("可");
+            }
+            else if(suuji < 60)
+            {
+                Console.WriteLine("不可");
+            }
+        }
+    }
+}
