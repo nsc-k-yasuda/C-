ceb9d5418d6cbad825c5656a1f84a5263d07e65b
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-02-28 20:06:46 +0900

    prob3-14.(難易度：★★)

---
+++ Program19.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace prob3_13
+{
+    class Program19
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("a,bに1から10までの数値を入力してください。");
+            Console.Write("a=");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("b=");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            if(a < 1 || a>10 || b < 1 || b>10)
+            {
+                Console.WriteLine("範囲外です。");
+            }
+            else if (a > b)
+            {
+                Console.WriteLine("aのほうが大きいです。");  //  正の数だった場合に実行
+            }
+            else if(a < b)
+            {
+                Console.WriteLine("bのほうが大きいです。");
+            }
+            else
+            {
+                Console.WriteLine("等しいです。");
+            }
+        }
+    }
+}
