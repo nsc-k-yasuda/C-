e18c3639d02dcc72ed3c92461362a7ecbfc48ab1
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-02-28 20:33:50 +0900

    prob3-10.(難易度：★)2/28

---
+++ Program15.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace prob3_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("文字１を入力：");
+            string str1 = Console.ReadLine();
+            Console.Write("文字２を入力：");
+            string str2 = Console.ReadLine();
+            if(str1 == str2)
+            {
+                Console.WriteLine("２つの文字列は等しい");
+            }
+            else
+            {
+                Console.WriteLine("２つの文字列は等しくない");
+            }
+        }
+    }
+}
