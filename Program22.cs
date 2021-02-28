9dfece2397a3ef7af51a3d6f18fa3986329d3e25
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-02-28 22:54:31 +0900

    prob3-17.(難易度：★★)2/28

---
+++ Program22.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace prob3_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("整数を入力してください(1-100)：");
+            int seisuu = int.Parse(Console.ReadLine());
+
+            if(seisuu < 0 || seisuu > 100)
+            {
+                Console.WriteLine("適切な値を入力してください。");
+            }else if (seisuu <= 50)
+            {
+                Console.WriteLine("50以下です。");
+            }else if(seisuu %2 == 0)
+            {
+                Console.WriteLine("偶数です。");
+            }else if (seisuu <= 50 && seisuu % 2 == 0)
+            {
+                Console.WriteLine("50以下の偶数です。");
+            }
+        }
+    }
+}
