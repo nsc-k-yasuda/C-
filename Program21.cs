1a0d9a46d5794d0fd0f9e2d4a491c2b276b40384
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-02-28 22:35:55 +0900

    prob3-16.(難易度：★★)2/28

---
+++ Program21.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace prob3_16
+{
+    class Program21
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("整数値を入力：");
+            int seisuu = int.Parse(Console.ReadLine());
+
+            if (seisuu % 2 == 0 && seisuu % 3 == 0)
+            {
+                Console.WriteLine("2と3の公倍数です。");
+            }
+            else if (seisuu % 2 == 0)
+            {
+                Console.WriteLine("2の倍数です。");
+            }else if (seisuu % 3 == 0)
+            {
+                Console.WriteLine("3倍数です。");
+            }
+        }
+    }
+}
