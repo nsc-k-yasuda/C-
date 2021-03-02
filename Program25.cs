2abb637f45b58f7ff2fb4d7c4cce8cdf4f1787d5
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-03-03 08:26:00 +0900

    prob4-2.(難易度：★)3/3

---
+++ Program25.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace prob4_2
+{
+    class Program25
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("数：");
+            int s = int.Parse(Console.ReadLine());
+            int i = 0;
+            while (i < s)
+            {
+                Console.Write("□ ");
+                i++;
+            }
+            Console.WriteLine();
+        }
+    }
+}
