17fa768504e759a4352b411cfe6b57447583b8c8
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-03-03 09:01:49 +0900

    prob4-4(難易度：★)3/3

---
+++ Program27.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prob4_4
+{
+    class Program27
+    {
+        static void Main(string[] args)
+        {
+            int i = 3;
+            while (i >= 0)
+            {
+                Console.WriteLine("num = {0}", i);
+                i--;
+            //break;
+            }
+            Console.WriteLine();
+        }
+    }
+}
