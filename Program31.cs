d802a643aca9e61ca66b1456fde8df1b0e37a0c6
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-03-04 01:43:02 +0900

    prob4-8.(難易度：★★)3/3

---
+++ Program31.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace prob4_8
+{
+    class Program31
+    {
+        static void Main(string[] args)
+        {
+            //int suuji = 0;
+            while (true)
+            {
+                Console.Write("0以上の数値を入力してください:");
+                int suuji = int.Parse(Console.ReadLine());
+                if(suuji < 0)
+                {
+                Console.WriteLine("終了します");
+                break;
+
+                }
+                else
+                {
+                    Console.WriteLine(""+suuji);
+
+                }
+            }
+        }
+    }
+}
