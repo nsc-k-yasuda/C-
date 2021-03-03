4f15540d0a85e23d557661ec33c97d2936ecdd18
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-03-04 01:49:25 +0900

    prob4-7(難易度：★)3/3

---
+++ Program30.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace prob4_7
+{
+    class Program30
+    {
+        static void Main(string[] args)
+        {
+            for(int a = 2; a < 10; a += 2)
+            {
+                Console.WriteLine("a = " + a);
+
+            }
+            //
+            //while ()
+            //{
+            //    
+            //}
+        }
+    }
+}
