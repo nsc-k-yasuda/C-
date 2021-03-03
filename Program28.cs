5d0d8c60253b714fcc72dc93776e1a7c6ad104f5
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-03-03 22:08:40 +0900

    prob4-5(難易度：★)3/3

---
+++ Program28.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace prob4_5
+{
+    class Program28
+    {
+        static void Main(string[] args)
+        {
+            //int i = 3;
+            for (int i = 3; i >= 0; i--)
+            {
+                Console.WriteLine("num = {0}", i);
+            }
+            Console.WriteLine();
+        }
+    }
+}
