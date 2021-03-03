3cb915907e0094d893522a03245a1934b9770327
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-03-04 01:18:32 +0900

    prob4-6(難易度：★)3/3

---
+++ Program29.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace prob4_6
+{
+    class Program29
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            while(a < 10){
+                Console.WriteLine("a = {0}" + a);
+                    a += 2 ;
+            }
+        }
+    }
+}
