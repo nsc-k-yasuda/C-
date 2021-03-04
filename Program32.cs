8089cc0c96ad0ca7e787a05f2f2aaf6aeb9f513c
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-03-04 22:16:15 +0900

    prob4-9.(難易度：★★)3/4

---
+++ Program32.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace prob4_9
+{
+    class Program32
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            while (true)
+            {
+                int dice = rnd.Next(1, 101);
+                Console.WriteLine(dice);
+                if (dice % 10 ==0)
+                {
+                    break;      //  ループから抜ける
+                }
+            }
+            Console.WriteLine("終了します");
+        }
+    }
+}
