dd470d28d73e6de9bf9e632208e606fa2a6aeaf5
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-03-18 08:56:31 +0900

    prob4-13.(難易度：★★)3/17

---
+++ Program36.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+
+namespace prob4_13
+{
+    class Program36
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            for (int i = 1; i <= 1; i++)
+            {
+                int a = rnd.Next(1, 11);
+                //Console.Write(a);
+                if(a >= 5)
+                {
+                    Console.WriteLine("★ ");
+                }
+                else
+                {
+                    Console.WriteLine("発生した数値：",a);
+                    Console.Write("☆ ");
+                }
+            }
+        }
+    }
+}
