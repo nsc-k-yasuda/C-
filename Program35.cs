c73343b6d535a79ebfaa567d82fca3752864b854
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-03-18 08:06:37 +0900

    prob4-12.(難易度：★★)3/17
    偶数奇数の個数が数えられない

---
+++ Program35.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace prob4_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                i++;
+                List<int> rndlist = new List<int>();
+                Random rnd = new Random();
+                for (int i = 1; i <= 5; i++)
+                {
+                    int a = rnd.Next(1, 101);
+                    //Console.Write(a);
+                    rndlist.Add(a);
+                    if (a % 2 == 0)
+                    {
+                        Console.WriteLine(a);
+                    }
+                    else
+                    {
+                        Console.WriteLine(a);
+                    }
+                    break;
+                }
+            Console.WriteLine("{i}番目",i);
+            }
+        }
+    }
+}
