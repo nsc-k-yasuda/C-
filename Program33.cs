cde39749ff2341384c17be6538decb4ccce8b6a4
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-03-18 07:51:54 +0900

    prob4-10.(難易度：★)

---
+++ Program33.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace prob4_10
+{
+    class Program33
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine(rnd.Next(101));
+            }
+            Console.WriteLine();
+        }
+    }
+}
