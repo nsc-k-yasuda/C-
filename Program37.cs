68a37d93f289c638a7070c4a695e25378b937c9b
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-03-19 21:30:52 +0900

    prob4-14.(難易度：★★)3/19

---
+++ Program37.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace prob4_14
+{
+    class Program37
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int dice = rnd.Next(1, 11);
+            for (int i = 0; i < dice; i++)
+            {
+            if (dice %2 == 0)
+            {
+                Console.Write("★");
+            }
+            else
+            {
+                Console.Write("☆");
+            }
+            }
+        }
+    }
+}
