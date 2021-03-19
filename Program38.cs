ed969184ea2c0ff40b84a7c49546263278c3b527
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-03-19 21:31:26 +0900

    prob4-15.(難易度：★★)3/19

---
+++ Program38.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace prob4_15
+{
+    class Program38
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 101; i++)
+            {
+                if (i % 10 ==0)
+                {
+                    Console.WriteLine(i);
+                }
+                else
+                {
+                    Console.Write(i);
+                }
+            }
+        }
+    }
+}
