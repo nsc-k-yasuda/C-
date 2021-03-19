8419b17543cd17772b32254c63f14837a82a57d3
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-03-19 21:31:55 +0900

    prob4-16.(難易度：★)3/19

---
+++ Program39.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace prob4_16
+{
+    class Program39
+    {
+        static void Main(string[] args)
+        {
+            int val = 0;
+            for (int i = 1; i <10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    val = i * j;
+                    Console.Write("{0}*{1}={2} ",i,j,i*j);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
