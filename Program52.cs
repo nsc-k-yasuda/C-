b012bd0e2d27325642c249506847f0a5aeaff689
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-03-23 22:57:56 +0900

    prob5-13.(難易度：★★)3/23

---
+++ Program52.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace prob5_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random rnd = new Random();
+            int[,] data = new int[3, 3];
+            int a, b;
+            List<int> rndlist = new List<int>();
+            for (a = 0; a < data.GetLength(0); a++)
+            {
+                for (b = 0; b < data.GetLength(1); b++)
+                {
+                    data[a, b] = rnd.Next(0, 10);
+                    rndlist.Add(data[a, b]);
+                }
+            }
+
+            for (a = 0; a < data.GetLength(0); a++)
+            {
+                for (b = 0; b < data.GetLength(1); b++)
+                {
+                    Console.Write("data[ {0} ]  [ {1} ] = {2} ", a, b, data[a, b]);
+
+                }
+            }
+                Console.WriteLine();
+                int min = rndlist.Min();
+                int max = rndlist.Max();
+                Console.WriteLine("最大値：" + max);
+                Console.WriteLine("最小値：" + min);
+
+        }
+    }
+}
