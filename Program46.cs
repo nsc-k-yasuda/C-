713d86724e75f93eedd66e5a7108f0ba60b6695e
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-03-23 23:02:50 +0900

    prob5-7.(難易度：★★★)3/23

---
+++ Program46.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace prob5_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] d = new int[5];
+            int sum, avg;
+            sum = 0;
+            for (int i = 0; i < d.Length; i++)
+            {
+                int a = rnd.Next(11);
+                Console.Write(" {0}",d[i]=a);
+                sum += d[i];
+            }
+            Console.WriteLine();
+            avg = sum / d.Length;
+            Console.WriteLine("合計値：" + sum);
+            Console.WriteLine("平均値：" + avg);
+
+            if (d > avg)
+            {
+                Console.WriteLine("平均値より大きい数：{0}" ,);
+            }
+        }
+    }
+}
