f13f085d8b0d0406579690151cb2d7394850bcc7
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-03-23 23:03:09 +0900

    prob5-6.(難易度：★★)3/23

---
+++ Program45.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace prob5_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] data = new int[10];
+            int sum,avg;
+            sum = 0;
+            int x;
+            List<int> rndlist = new List<int>();
+            for (int i = 0; i < data.Length; i++)
+            {
+                x = rnd.Next(1,11);
+                Console.Write("{0} ",data[i]=x);
+                sum += data[i];
+                rndlist.Add(x);
+            }
+            Console.WriteLine();
+            int min = rndlist.Min();
+            int max = rndlist.Max();
+            avg = sum / data.Length;
+            Console.WriteLine("最大値：" + max);
+            Console.WriteLine("最小値：" + min);
+            Console.WriteLine("平均値：" + avg);
+        }
+    }
+}
