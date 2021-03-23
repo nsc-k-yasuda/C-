904a2cd997b26ee5ae742969e33fc92efaec37c2
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-03-23 23:03:26 +0900

    prob5-3.(難易度：★★)

---
+++ Program42.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace prob5_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] a = new int[10];
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = rnd.Next(1, 101);
+                Console.Write("{0} ", a[i]);
+            }
+
+            //    Random r = new Random();
+            //    int[] data = new int[10];
+
+            //    for (int i = 0; i < 10; i++)
+            //    {
+            //        data[i] = r.Next(100) + 1;
+            //        Console.Write("{0} ", data[i]);
+            //    }
+
+            //    Console.Write("\r\n\r\n奇数：");
+            //    for (int i = 0; i < 10; i++)
+            //        if (data[i] % 2 != 0) Console.Write("{0} ", data[i]);
+            //    Console.Write("\r\n偶数：");
+            //    for (int i = 0; i < 10; i++)
+            //        if (data[i] % 2 == 0) Console.Write("{0} ", data[i]);
+            //}
+        }
+    }
+}
