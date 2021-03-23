c923394cb6c176cfe45265f3fcff36e984404c9b
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-03-23 23:03:56 +0900

    prob5-1.(難易度：★)3/23

---
+++ Program40.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace prob5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] a = new int[7];
+            int s=-1;
+            for (int i = 0; i < a.Length; i++)
+            {
+                int dice = rnd.Next(1, 11);
+                s = s + 1;
+                Console.Write(" a[{0}]={1} ",s,dice);
+            }
+        }
+    }
+}
