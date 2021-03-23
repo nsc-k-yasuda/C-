4f3d2db26f91d5f6c67707d3c389a4ba6a44db06
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-03-23 23:02:14 +0900

    prob5-8.(難易度：★★)3/23

---
+++ Program47.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prob5_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] data = new int[5];
+            for (int i = 0; i <= 5; i++)
+            {
+                data[i] = rnd.Next(-10,10)+1;
+                Console.Write("{0} ", data[i]);
+
+            }
+            Console.Write("\r\n\r\n0より大きい数：");
+            for (int i = 0; i < 5; i++)
+                if (data[i] > 0) Console.Write("{0} ", data[i]);
+            Console.Write("\r\n0より小さい数：");
+            for (int i = 0; i < 5; i++)
+                if (data[i] < 0) Console.Write("{0} ", data[i]);
+            Console.Write("\r\n0の数：");
+            for (int i = 0; i < 5; i++)
+                if (data[i] == 0) Console.Write("{0} ", data[i]);
+        }
+    }
+}
