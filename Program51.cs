632abe92b8d7478bb497701bfcb727e3772b3fe7
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-03-23 23:01:48 +0900

    prob5-12.(難易度：★)3/23

---
+++ Program51.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace prob5_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            int [ , ]  data = new int [ 3, 3 ];
+            int a, b;
+
+
+            for (a = 0; a < data.GetLength(0); a++)
+            {
+
+                for (b = 0; b < data.GetLength(1); b++)
+                {
+                    data[a, b] = rnd.Next(0, 10);
+                }
+            }
+
+            for (a = 0; a < data.GetLength(0); a++)
+            {
+                for (b = 0; b < data.GetLength(1); b++)
+                {
+                    Console.Write( "data[ {0} ]  [ {1} ] = {2} ", a , b , data[ a, b ] );
+
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
