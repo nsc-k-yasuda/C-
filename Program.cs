b89caae79980ca692652bd383d3c51a08c8aadad
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-03-23 22:53:12 +0900

    prob5-12.(難易度：★)

--- Program.cs
+++ Program.cs
@@ -1,32 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace prob5_8
+namespace prob5_12
 {
     class Program
     {
         static void Main(string[] args)
         {
             Random rnd = new Random();
-            int[] data = new int[5];
-            for (int i = 0; i <= 5; i++)
-            {
-                data[i] = rnd.Next(-10,10)+1;
-                Console.Write("{0} ", data[i]);
 
+            int [ , ]  data = new int [ 3, 3 ];
+            int a, b;
+
+
+            for (a = 0; a < data.GetLength(0); a++)
+            {
+
+                for (b = 0; b < data.GetLength(1); b++)
+                {
+                    data[a, b] = rnd.Next(0, 10);
+                }
             }
-            Console.Write("\r\n\r\n0より大きい数：");
-            for (int i = 0; i < 5; i++)
-                if (data[i] > 0) Console.Write("{0} ", data[i]);
-            Console.Write("\r\n0より小さい数：");
-            for (int i = 0; i < 5; i++)
-                if (data[i] < 0) Console.Write("{0} ", data[i]);
-            Console.Write("\r\n0の数：");
-            for (int i = 0; i < 5; i++)
-                if (data[i] == 0) Console.Write("{0} ", data[i]);
+
+            for (a = 0; a < data.GetLength(0); a++)
+            {
+                for (b = 0; b < data.GetLength(1); b++)
+                {
+                    Console.Write( "data[ {0} ]  [ {1} ] = {2} ", a , b , data[ a, b ] );
+
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
