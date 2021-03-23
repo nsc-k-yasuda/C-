a76eccdd464196640610bf84ee96091bced4ade2
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-03-23 22:51:48 +0900

    prob5-3.(難易度：★★)3/23

--- Program.cs
+++ Program.cs
@@ -1,23 +1,35 @@
 ﻿using System;
 
-namespace prob5_2
+namespace prob5_3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            double[] d = new double[4];
-            d[0] = 0.2;
-            d[1] = -5.1;
-            d[2] = 3.2;    //  変数の代入
-            d[3] = 1.8;
-            int suuji = -1;
-            for (int i = 0; i < d.Length; i++)
+            Random rnd = new Random();
+            int[] a = new int[10];
+            for (int i = 0; i < a.Length; i++)
             {
-                suuji += 1;
-                Console.Write(" d[{0}]={1} ",suuji , d[i]);
+                a[i] = rnd.Next(1, 101);
+                Console.Write("{0} ", a[i]);
             }
-            Console.WriteLine();
+
+            //    Random r = new Random();
+            //    int[] data = new int[10];
+
+            //    for (int i = 0; i < 10; i++)
+            //    {
+            //        data[i] = r.Next(100) + 1;
+            //        Console.Write("{0} ", data[i]);
+            //    }
+
+            //    Console.Write("\r\n\r\n奇数：");
+            //    for (int i = 0; i < 10; i++)
+            //        if (data[i] % 2 != 0) Console.Write("{0} ", data[i]);
+            //    Console.Write("\r\n偶数：");
+            //    for (int i = 0; i < 10; i++)
+            //        if (data[i] % 2 == 0) Console.Write("{0} ", data[i]);
+            //}
         }
     }
 }
