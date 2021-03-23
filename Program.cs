6437cb539e3d906b0cbcf83b736f584c523f66e5
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-03-23 22:52:14 +0900

    prob5-6.(難易度：★★)3/23

--- Program.cs
+++ Program.cs
@@ -1,35 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace prob5_3
+namespace prob5_6
 {
     class Program
     {
         static void Main(string[] args)
         {
             Random rnd = new Random();
-            int[] a = new int[10];
-            for (int i = 0; i < a.Length; i++)
+            int[] data = new int[10];
+            int sum,avg;
+            sum = 0;
+            int x;
+            List<int> rndlist = new List<int>();
+            for (int i = 0; i < data.Length; i++)
             {
-                a[i] = rnd.Next(1, 101);
-                Console.Write("{0} ", a[i]);
+                x = rnd.Next(1,11);
+                Console.Write("{0} ",data[i]=x);
+                sum += data[i];
+                rndlist.Add(x);
             }
-
-            //    Random r = new Random();
-            //    int[] data = new int[10];
-
-            //    for (int i = 0; i < 10; i++)
-            //    {
-            //        data[i] = r.Next(100) + 1;
-            //        Console.Write("{0} ", data[i]);
-            //    }
-
-            //    Console.Write("\r\n\r\n奇数：");
-            //    for (int i = 0; i < 10; i++)
-            //        if (data[i] % 2 != 0) Console.Write("{0} ", data[i]);
-            //    Console.Write("\r\n偶数：");
-            //    for (int i = 0; i < 10; i++)
-            //        if (data[i] % 2 == 0) Console.Write("{0} ", data[i]);
-            //}
+            Console.WriteLine();
+            int min = rndlist.Min();
+            int max = rndlist.Max();
+            avg = sum / data.Length;
+            Console.WriteLine("最大値：" + max);
+            Console.WriteLine("最小値：" + min);
+            Console.WriteLine("平均値：" + avg);
         }
     }
 }
