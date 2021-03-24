eca3c08cbb10d6933de84c75fd7eac9500124e56
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-03-24 18:15:14 +0900

    prob5-9.(難易度：★★)

---
+++ Program48.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace prob5_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            Random rnd = new Random();
+            int[] a = new int[10];
+            List<int> rndlist = new List<int>();
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = rnd.Next(-1, 101);
+                Console.Write("{0} ",a[i]);
+                rndlist.Add(a[i]);
+
+                if (a[i] == 0)
+                {
+                    count += 1;
+                }
+                else if (a[i] >= 50)
+                {
+                    Console.WriteLine("50以上:{0}", a[i]);
+                }
+                else
+                {
+                    Console.WriteLine("50未満:{0}", a[i]);
+
+                }
+            }
+            Console.WriteLine("0の個数：{0}",count);
+        }
+    }
+}
