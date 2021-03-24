a866fec54485bcd55c7f33c4e055244e3122742b
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-03-24 18:13:49 +0900

    prob5-4.(難易度：★★)3/24

---
+++ Program43.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace prob5_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] a = new int[10];
+            List<int> rndlist = new List<int>();
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = rnd.Next(1, 101);
+                Console.Write(a[i] + " ");
+                rndlist.Add(a[i]);
+                if (a[i] >= 50)
+                {
+                    Console.WriteLine("50以上:{0}",a[i]);
+                }
+                else
+                {
+                    Console.WriteLine("50未満:{0}", a[i]);
+                }
+            }
+            Console.WriteLine();     
+        }
+    }
+}
