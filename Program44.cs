16648861ed045404be0c0d8c795720c9dcbd6a62
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-03-24 18:14:41 +0900

    prob5-5.(難易度：★★)3/24

---
+++ Program44.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace prob5_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] a = new int[10];
+            List<int> rndlist = new List<int>();
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = rnd.Next(1, 101);
+                Console.Write(a[i] + " ");
+                rndlist.Add(a[i]);
+                if (a[i] % 3 ==0)
+                {
+                    Console.WriteLine("3の倍数：{0} ", a[i]);
+                }
+                else
+                {
+                    Console.WriteLine("3の倍数以外の数：{0}", a[i]);
+                }
+            }
+        }
+    }
+}
