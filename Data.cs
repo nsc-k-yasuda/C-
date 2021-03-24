cb180c4af90dd671ea8e98484a00650ae989e995
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-03-24 18:12:17 +0900

    prob7-1.(難易度★)3/24

---
+++ Data.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace prob7_1
+{
+    class Data
+    {
+        //  メンバ変数number
+        private int number = 100;
+        //  メンバ変数comment
+        private string comment = "";
+        public void Datas()
+        {
+            Console.WriteLine("number = {0} ", number);
+            Console.WriteLine("comment = {0}", comment);
+        }
+        //  data1のプロパティ（読み込みオンリー）
+        public int Number
+        {
+            get { return number; }
+        }
+        //  data2のプロパティ（書き込みオンリー）
+        public string Comment
+        {
+            set { comment = value; }
+        }
+    }
+}
---
+++ Program56.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace prob7_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Data d = new Data();
+            //d.Number = 100;
+            d.Comment = "Programming C#";
+            d.Datas();
+            //Console.WriteLine("number = " + d.Number);
+            //Console.WriteLine("comment = " + d.Comment);
+        }
+    }
+}
