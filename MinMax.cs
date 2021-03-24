7ecdb0d9b64b7685839d660378a6e0207b1ad13e
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-03-24 12:59:33 +0900

    prob6-1.(難易度★)

---
+++ MinMax.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace prob6_1
+{
+    class MinMax
+    {
+        public int Max(int n1, int n2, int n3)
+        {
+            if (n1 > n2 && n1 > n3)
+            {
+                return n1;
+            }else if(n2 > n3)
+            {
+                return n2;
+            }else
+            {
+                return n3;
+            }
+        }
+
+        public int Min(int n1, int n2, int n3)
+        {
+            if (n2 > n1 && n3 > n1)
+            {
+                return n1;
+            }
+            else if (n2 > n3)
+            {
+                return n3;
+            }
+            else
+            {
+                return n2;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,41 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
-namespace prob5_13
+namespace prob6_1
 {
     class Program
     {
         static void Main(string[] args)
         {
-
-            Random rnd = new Random();
-            int[,] data = new int[3, 3];
-            int a, b;
-            List<int> rndlist = new List<int>();
-            for (a = 0; a < data.GetLength(0); a++)
-            {
-                for (b = 0; b < data.GetLength(1); b++)
-                {
-                    data[a, b] = rnd.Next(0, 10);
-                    rndlist.Add(data[a, b]);
-                }
-            }
-
-            for (a = 0; a < data.GetLength(0); a++)
-            {
-                for (b = 0; b < data.GetLength(1); b++)
-                {
-                    Console.Write("data[ {0} ]  [ {1} ] = {2} ", a, b, data[a, b]);
-
-                }
-            }
-                Console.WriteLine();
-                int min = rndlist.Min();
-                int max = rndlist.Max();
-                Console.WriteLine("最大値：" + max);
-                Console.WriteLine("最小値：" + min);
-
+            MinMax m = new MinMax();
+            int a = 4, b = 2, c=10;
+            Console.WriteLine("{0}と{1}と{2}のうち、最大のものは{3}", a, b, c, m.Max(a, b, c));
+            Console.WriteLine("{0}と{1}と{2}のうち、最小のものは{3}", a, b, c, m.Min(a, b, c));
         }
     }
 }
