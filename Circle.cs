09c13e6b0501422e3b44428d5ab912abd1c1c386
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-03-24 18:11:35 +0900

    prob6-3.(難易度★)3/24

---
+++ Circle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace prob6_3
+{
+    class Circle
+    {
+        //  半径
+        public double r;
+        //  円周の長さを求める
+        public double Circumference()
+        {
+            return 2 * 3.14 * r;
+        }
+        public double Area()
+        {
+            return r * r * 3.14 ;
+        }
+    }
+}
---
+++ Program55.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace prob6_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle c = new Circle();
+            //  円の半径を設定
+            c.r = 4.0;
+            Console.WriteLine("半径" + c.r + "の円の円周の長さは" + c.Circumference());
+            Console.WriteLine("半径" + c.r + "の円の面積の長さは" + c.Area());
+        }
+    }
+}
