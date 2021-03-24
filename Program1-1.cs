eb04221abc1d8aec5e0dc619b88b284615c12987
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-03-25 00:16:13 +0900

    応用編prob1-1.(難易度：★)3/25

---
+++ Program1-1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace prob1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sample s = new Sample();
+            s.Constracor();
+            s.foo();
+        }
+    }
+}
