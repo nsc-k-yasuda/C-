c3c31e62535cfd08738ab71fdd8ea159f97e1e46
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-03-24 18:13:01 +0900

    prob7-3.(難易度★)

---
+++ Calculation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace prob7_3
+{
+    class Calculation
+    {
+        private int number1;
+        //  二つ目の文字列
+        private int number2;
+        //  一つ目の文字列を設定
+        public int Number1
+        {
+            set { number1 = value; }
+            get { return number1; }
+        }
+        //  一つ目の文字列を設定
+        public int Number2
+        {
+            set { number2 = value; }
+            get { return number2; }
+        }
+        public int Add()
+        {
+            return number1 + number2;
+        }
+        public int Sub()
+        {
+            return number1 - number2;
+        }
+    }
+}
---
+++ Program58.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace prob7_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculation c = new Calculation();
+            c.Number1 = 8;    //  一つ目の数をセット
+            c.Number2 = 9;    //  二つ目の数をセット
+            //  二つの数の和を表示
+            Console.WriteLine("{0} + {1} = {2}", c.Number1, c.Number2, c.Add());
+            //  二つの数の差を表示
+            Console.WriteLine("{0} - {1} = {2}", c.Number1, c.Number2, c.Sub());
+        }
+    }
+}
