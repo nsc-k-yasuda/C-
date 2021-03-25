f7d3495aa271be515e5be00f54752ccfcc41adca
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-03-25 20:30:35 +0900

    応用問題Problemex1_1_3/25

---
+++ Sample59.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace prob1_1
+{
+    class Sample
+    {
+        public Sample()
+        {
+            Console.WriteLine("コンストラクタ");
+        }
+        public void foo()
+        {
+            Console.WriteLine("foo");
+        }
+
+    }
+}
