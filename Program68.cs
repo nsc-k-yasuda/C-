67473ee8b07d35459d8084023485ce33f7a418b5
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-03-29 19:05:23 +0900

    probex6-8.(難易度：★)3/28

---
+++ Program68.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace probex6_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("英語で動物の名前を入力してください：");
+            string a = Console.ReadLine();
+            //  連想記憶クラスの生成
+            Dictionary<String, String> animal = new Dictionary<String, String>();
+
+            //  データの追加
+            animal["cat"] = "猫";
+            animal["dog"] = "犬";
+            animal["bird"] = "鳥";
+            animal["tiger"] = "トラ";
+
+            if(a == "cat") { 
+                  Console.WriteLine("「{0}」です。", animal); 
+            }else if(a == "dog")
+            {
+                Console.WriteLine("「{0}」です。",  animal);
+            }
+            else if (a == "bird")
+            {
+                Console.WriteLine("「{0}」です。",  animal);
+            }
+            else if (a == "tiger")
+            {
+                Console.WriteLine("「{0}」です。",  animal);
+            }else
+            {
+                Console.WriteLine("対応するデータは登録されていません。");
+            }
+            
+        }
+    }
+}
