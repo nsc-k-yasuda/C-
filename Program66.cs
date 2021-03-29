ee1ad2458643afa1c52d5bb661e1665b75274984
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-03-29 19:04:59 +0900

    probex6-6.(難易度：★)3/28

---
+++ Program66.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace probex6_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            List<int> even = new List<int>();
+            
+            while (true)
+            {
+                int i = r.Next(11);
+
+                Console.WriteLine($"0～10の値を出力:{i}");
+
+                if (i == 0)
+                {
+                    break;
+                }
+                even.Add(i);
+                if (even.Contains(2))
+                {
+                    even.RemoveAt(2);
+                }
+            }
+            foreach (int s in even)
+            {
+                Console.Write("{0} ",s);
+            }
+        }
+    }
+}
