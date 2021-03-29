587b3c13afe5c3815fef3b642cff6517dba08f89
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-03-29 18:56:58 +0900

    probex6-1.(難易度：★)3/28

---
+++ Program61.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace probex6_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            List<int> even = new List<int>();
+            List<int> odd = new List<int>();
+
+            while (true)
+            {
+                int i = r.Next(11);
+
+                Console.WriteLine($"0～10の値を出力:{i}");
+
+                if (i == 0)
+                {
+                    break;
+                }
+
+                if (i % 2 == 0)
+                {
+                    even.Add(i);
+                }
+                else
+                {
+                    odd.Add(i);
+                }
+            }
+
+            string answer(List<int> data)
+            {
+                string ans = string.Empty;
+
+                for (int i = 0; i < data.Count; i++)
+                {
+                    ans += data[i];
+
+                    if (i < data.Count - 1)
+                    {
+                        ans += ' ';
+                    }
+                }
+
+                return ans;
+            }
+
+            Console.WriteLine($"偶数:{answer(even)}");
+            Console.WriteLine($"奇数:{answer(odd)}");
+        }
+    }
+}
