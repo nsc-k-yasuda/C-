1b3106ad0168921ebb59d49b8353989e35bff796
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-03-29 19:07:14 +0900

    probx7-3.(難易度：★)3/29

---
+++ Program73.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Problemex7_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int[] a = { 0, 1, 2 };
+                //  配列の内容を表示
+                for (int i = 0; i < 4; i++)
+                {
+                    Console.WriteLine("a[" + i + "]=" + a[i]);
+                }
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine("配列の範囲を超えています。");
+            }
+        }
+    }
+}
