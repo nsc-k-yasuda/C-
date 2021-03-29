02f71160e219f51f11eba4b5c1bc04f973284e23
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-03-29 19:05:43 +0900

    probex6-10.(難易度：★)3/28

---
+++ Program70.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace probex6_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random a = new Random();
+            int b = 0;
+            //  ハッシュセットの生成
+            HashSet<int> t = new HashSet<int>();
+            for (int i = 0; i <= 10; i++)
+            {
+                b = a.Next(1, 11);
+
+            Console.Write("乱数: {0} ",b);
+                t.Add(b);
+            }
+            Console.WriteLine();
+            //データの出力
+            foreach (int i in t)
+            {
+                Console.Write("出現した数: {0}", i);
+            }
+        }
+    }
+    
+}
