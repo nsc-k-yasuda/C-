616f362ed0b2726dd6a8e2cd5fb1af60d61760fb
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-03-29 19:02:33 +0900

    probex6-5.(難易度：★★)3/28

---
+++ Program65.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace probex6_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> a = new List<string>();
+            int c = 0;
+            while (true)
+            {
+                Console.Write("文字列を入力:");
+                a.Add(Console.ReadLine());
+                if (a[c] == "")
+                {
+                    break;
+                }
+                c++;
+            }
+            Console.WriteLine();
+            foreach (string i in a)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
