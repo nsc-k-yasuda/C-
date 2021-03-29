c8498958875cd2fb3f2850f6636350b4e6079e6f
nsc-k-yasuda <63280689+nsc-k-yasuda@users.noreply.github.com>
2021-03-29 19:06:38 +0900

    probx7-2.(難易度：★)3/28

---
+++ Program72.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Problemex7_2
+{
+    //  デリゲート
+    delegate void Delegatefunc(int num);
+    class Showcharas
+    {
+        //  星を表示
+        public void ShowStars(int num)
+        {
+            for (int i = 0; i < num; i++)
+            {
+                Console.Write("☆");
+            }
+            Console.WriteLine();
+        }
+        //  資格を表示
+        public void ShowBoxes(int num)
+        {
+            for (int i = 0; i < num; i++)
+            {
+                Console.Write("■");
+            }
+            Console.WriteLine();
+        }
+        //  +を表示
+        public void ShowPlus(int num)
+        {
+            for (int i = 0; i < num; i++)
+            {
+                Console.Write("＋");
+            }
+            Console.WriteLine();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Showcharas s = new Showcharas();
+            Delegatefunc f = new Delegatefunc(s.ShowStars);
+            f += new Delegatefunc(s.ShowBoxes);
+            f += new Delegatefunc(s.ShowPlus);
+            f(4);
+
+        }
+    }
+}
